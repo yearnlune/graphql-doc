0067356200f5e5e3b9374fb6cd2214124fe8173b
DongHwan Kim <kdhpopoya@gmail.com>
2024-01-04 17:02:58 +0900

    fix: Calculated relative paths

--- create-graphql-doc/src/index.ts
+++ create-graphql-doc/src/index.ts
@@ -1,17 +1,18 @@
 import prompts from 'prompts';
 import path from 'path';
-import shell from 'shelljs'
-import fs from 'fs-extra'
+import shell from 'shelljs';
+import fs from 'fs-extra';
+import logger from '@docusaurus/logger';
 
 export default async function init(
         directory: string,
 ): Promise<void> {
   const docPath = await getDocPath();
   const dest = path.resolve(directory, docPath);
-  const graphqlPaths = await getGraphqlPaths();
-  shell.exec(`git clone --recursive --depth 1 https://github.com/yearnlune/graphql-doc-template.git ${dest}`)
-  await fs.remove(path.join(dest, '.git'))
+  const graphqlPaths = (await getGraphqlPaths()).map(v => calculateRelativePath(directory, docPath, v));
+  await getGraphqlDocTemplate(dest);
   await updateSchemaPath(path.join(dest, 'docusaurus.config.ts'), graphqlPaths);
+  logger.success`Created graphql doc=${dest}.`;
 }
 
 async function getDocPath(): Promise<string> {
@@ -20,7 +21,7 @@ async function getDocPath(): Promise<string> {
             type: 'text',
             name: 'docPath',
             message: 'Where would you like to create it?',
-            initial: './doc',
+            initial: 'doc',
             validate: (dir) => {
               if (!isRelativePath(dir)) {
                 return `Invalid file path: ${dir}`;
@@ -33,7 +34,7 @@ async function getDocPath(): Promise<string> {
               process.exit(1);
             },
           },
-  )) as { docPath: string };
+  )) as {docPath: string};
   return docPath;
 }
 
@@ -54,7 +55,7 @@ async function getGraphqlPath(): Promise<string> {
             type: 'text',
             name: 'graphqlPath',
             message: 'Where is the Graphql directory? (Relative path)',
-            initial: '/src/main/resources',
+            initial: 'src/main/resources',
             validate: async (dir: string) => {
               if (!isRelativePath(dir)) {
                 return `Invalid file path: ${dir}`;
@@ -67,8 +68,8 @@ async function getGraphqlPath(): Promise<string> {
               process.exit(1);
             },
           },
-  )) as { graphqlPath: string };
-  return graphqlPath
+  )) as {graphqlPath: string};
+  return graphqlPath;
 }
 
 async function isContinue(): Promise<boolean> {
@@ -79,28 +80,48 @@ async function isContinue(): Promise<boolean> {
             message: 'Do you want to continue?',
             active: 'yes',
             inactive: 'no',
-            initial: true
+            initial: true,
           },
           {
             onCancel() {
               process.exit(1);
             },
           },
-  )) as { isContinue: boolean };
+  )) as {isContinue: boolean};
   return isContinue;
 }
 
 async function updateSchemaPath(path: string, graphqlPaths: string[]) {
   const docusaurusConfig = (fs.readFileSync(path, 'utf-8'));
   await fs.outputFile(
-      path,
-      docusaurusConfig.replace(
-          `"schema" // graphql paths`,
-          graphqlPaths.map((v) => `"${v}",`).join(" ")
-      ),
-  )
+          path,
+          docusaurusConfig.replace(
+                  `"schema" // graphql paths`,
+                  graphqlPaths.map((v) => `"${v}",`).join(' '),
+          ),
+  );
+}
+
+async function getGraphqlDocTemplate(dest: string) {
+  shell.exec(`git clone --recursive --depth 1 https://github.com/yearnlune/graphql-doc-template.git ${dest}`);
+  await fs.remove(path.join(dest, '.git'));
 }
 
 function isRelativePath(path: string) {
   return RegExp('^[.\\/a-zA-Z0-9-_]+$').test(path);
+}
+
+function calculateRelativePath(root: string, source: string, target: string): string {
+  let result = '';
+  const rootTokens = root.split('\\');
+  source.split('/').forEach((token) => {
+    if (token == '..') {
+      const lastToken = rootTokens.pop();
+      result += lastToken + '/';
+    } else if (token == '.') {
+    } else {
+      result = '../' + result;
+    }
+  });
+  return result + target;
 }
